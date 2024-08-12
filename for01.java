8438a5d09c720b5a25499f52c35660967b9cb430
zpzpzp123 <130284287+zpzpzp123@users.noreply.github.com>
2024-08-12 09:31:03 +0800

    Get started with “for” loops
    Print all integers between 1 and 100 that are multiples of 9, counting the number and sum（Simplify, die before you live)
    打印1到100之间所有是9的倍数的整数，统计个数和总和（化繁为简，先死后活）

---
+++ for01.java
@@ -0,0 +1,18 @@
+public class for01{
+	public static void main(String[] args) {
+	        int numble =0;
+	        int sum =0;
+		for(int i=1;i<=100;i++){
+          if(i%9 == 0){
+              numble=numble+1;
+              sum = sum+i;
+          }
+          else{
+          	
+          }
+          
+		}
+		System.out.println("numble="+numble+"sum="+sum);
+
+	}
+}
